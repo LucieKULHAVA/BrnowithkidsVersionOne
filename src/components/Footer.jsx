3847f00ba6532fa796f9425ee0ab000e2f6905f1
Helena Ibarra <helena8ibarra@gmail.com>
2021-05-27 13:17:50 +0200

    zahajeni projektu

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer>
+        <h3>Where to find us</h3>
+        <div class="logo"></div>
+
+        <div class="footer__links">
+            <a href="https://www.facebook.com/VicinisNGO">
+                <img src="icons/FB icon.png" alt="facebook icon" />
+            </a>
+            <a href="mailto:info@vicinis.cz">
+                <img src="icons/envelop icon.png" alt="envelop icon" />
+            </a>
+            <img src="icons/chat icon.png" alt="chat icon" />
+        </div>
+    </footer>
+);
+
+export default Footer;
