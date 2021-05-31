08555e031df528cb65dc9c0cfebb438733e52a60
Helena Ibarra <helena8ibarra@gmail.com>
2021-05-31 15:28:19 +0200

    komponenta HealthCare, instal + import Router

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import MainPage from './components/Main/MainPage';
+import HealthCare from './components/HealthCare/HealthCare';
 
 import './style.css';
 
 const App = () => (
-  <div className="banner">
-    <Header />
+  <Router>
+    <div className="banner">
+      <Link to="/healthCare">Health Care</Link>
+      <Header />
 
-    <main className="container">
-      <MainPage />
-    </main>
-    <Footer />
-  </div>
+      <main className="container">
+        <Switch>
+          <Route exact path="/">
+            <MainPage />
+          </Route>
+          <Route path="/healthCare">
+            <HealthCare />
+          </Route>
+        </Switch>
+      </main>
+      <Footer />
+    </div>
+  </Router>
 );
 
 render(<App />, document.querySelector('#app'));
