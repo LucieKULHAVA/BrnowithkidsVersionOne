0c00a27c127d100ce09245f3d0faa59029f823ab
LucieKULHAVA <46672152+LucieKULHAVA@users.noreply.github.com>
2021-06-06 15:45:51 +0200

    přidáno Searchdoctors

---
+++ src/components/SearchDoctors/src/doctors.js
@@ -0,0 +1,68 @@
+export const facilities = [
+	{
+		id: 1,
+		name: 'Ahmed Amerovic',
+		specialisation: 'pediatrician',
+        languagues: 'serbian, czech',
+		cover: 'rocket.svg',
+		link: 'url',
+	},
+	{
+		id: 2,
+		name: 'Doktor Mengele',
+		specialisation: 'opthalmologist',
+        languagues: 'russian, english',
+		cover: 'rocket.svg',
+		link: 'www.google.com',
+	},
+	{
+		id: 3,
+		name: 'Doktor Mengele',
+		specialisation: 'opthalmologist',
+        languagues: 'german, czech',
+		cover: 'rocket.svg',
+		link: 'www.google.com',
+	},
+	{
+		id: 4,
+		name: 'Doktor Mengele',
+		specialisation: 'opthalmologist',
+        languagues: 'russian, english, czech, arabic',
+		cover: 'rocket.svg',
+		link: 'www.google.com',
+	},
+	{
+		id: 5,
+		name: 'Doktor Mengele',
+		specialisation: 'opthalmologist',
+        languagues: 'english, czech',
+		cover: 'rocket.svg',
+		link: 'www.google.com'
+	},
+	{
+		id: 6,
+		name: 'Doktor Mengele',
+		specialisation: 'opthalmologist',
+        languagues: 'german, czech',
+		cover: 'rocket.svg',
+		link: 'www.google.com'
+	},
+	{
+		id: 7,
+		name: 'Doktor Mengele',
+		specialisation: 'opthalmologist',
+        languagues: 'german, czech',
+		cover: 'rocket.svg',
+		link: 'www.google.com',
+	},
+	{
+		id: 8,
+		name: 'Doktor Mengele',
+		specialisation: 'opthalmologist',
+        languagues: 'german, czech',
+		cover: 'rocket.svg',
+		link: 'www.google.com',
+	},
+
+	
+];
