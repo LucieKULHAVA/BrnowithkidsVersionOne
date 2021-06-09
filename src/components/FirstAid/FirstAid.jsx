cd599f4e9d373e23b660f612b872d9a4e4f92db7
Helena Ibarra <helena8ibarra@gmail.com>
2021-06-09 23:57:44 +0200

    firstAid

--- src/components/FirstAid/FirstAid.jsx
+++ src/components/FirstAid/FirstAid.jsx
@@ -39,240 +39,240 @@ const FirstAid = () => {
             </li>
           </ul>
         </div>
-
-        <h3>Choking</h3>
-
-        <p>
-          This is a condition in which the airways are obstructed by, for
-          example, by a piece of food, or a small object swallowed by a child.
-          There are two types of obstruction:
-        </p>
-
-        <ul>
-          <li>
+        <div className="firstAid__tips">
+          <h3>Choking</h3>
+          <p>
+            This is a condition in which the airways are obstructed by, for
+            example, by a piece of food, or a small object swallowed by a child.
+            There are two types of obstruction:
+          </p>
+          <p>
             partial - the airways are not closed, air flow is possible. The most
             common defensive reflex of the body is coughing.
-          </li>
-          <li>
+          </p>
+          <p>
             total - foreign matter in the respiratory tract closes the lumen of
             the duct and cuts off the access of air. In this case, the injured
             person is not able to speak, cough or breathe. Initially, the
             audible whistling of air in the airways calms down and the victim is
             exposed to loss of consciousness.
-          </li>
-        </ul>
+          </p>
+          <h3>How can you help a choking infant?</h3>
 
-        <h4>How can you help a choking infant?</h4>
+          <p>
+            In the event that the cough is effective, you only need to watch the
+            child. If the child is conscious but not coughing or if the coughing
+            is not effective, then:
+          </p>
+          <ul>
+            <li>
+              1. kneel and place the child on your forearm with the head facing
+              downwards,
+            </li>
+            <li>
+              2. hold the child’s lower jaw with your thumb and index finger,
+              while tilting their head back and opening the airway (do not hold
+              their neck!),
+            </li>
+            <li>
+              3. perform 5 blows with the base of the wrist in the
+              inter-shoulder area,
+            </li>
+            <li>
+              4. after the impacts, check whether the foreign body has been
+              removed from the respiratory tract or whether it is not in the
+              mouth, from where it can be pulled out with a small finger.
+            </li>
+          </ul>
+          <p>If the above actions fail, you should:</p>
+          <ul>
+            <li>
+              1. turn the infant on their back on the other forearm (head still
+              down),
+            </li>
+            <li>2. embrace the baby's occiput with your hand,</li>
+            <li>
+              3. indicate the place to compress the chest (lower half of the
+              sternum, 1 finger wide above the end),
+            </li>
+            <li>4. perform 5 chest compressions.</li>
+          </ul>
+          <p>
+            Then repeat both steps alternately (5 strokes and 5 compressions)
+            until the foreign body is removed.
+          </p>
+          <p>
+            If the infant loses consciousness, resuscitation is required! Start
+            with 5 breaths to push the foreign body deeper, even if it falls
+            into one of the lungs, it will allow the other lung to breathe.
+          </p>
+          <h4>How do you help a child over 1 year old?</h4>
+          <p>
+            In the event that the cough results, you should only encourage the
+            cough and watch the child. If the child is conscious but not
+            coughing or coughing is not effective, then:
+          </p>
+          <ul>
+            <li>1. stand behind the child,</li>
 
-        <p>
-          In the event that the cough is effective, you only need to watch the
-          child. If the child is conscious but not coughing or if the coughing
-          is not effective, then:
-        </p>
-        <ul>
-          <li>
-            1. kneel and place the child on your forearm with the head facing
-            downwards,
-          </li>
-          <li>
-            2. hold the child’s lower jaw with your thumb and index finger,
-            while tilting their head back and opening the airway (do not hold
-            their neck!),
-          </li>
-          <li>
-            3. perform 5 blows with the base of the wrist in the inter-shoulder
-            area,
-          </li>
-          <li>
-            4. after the impacts, check whether the foreign body has been
-            removed from the respiratory tract or whether it is not in the
-            mouth, from where it can be pulled out with a small finger.
-          </li>
-        </ul>
-        <p>If the above actions fail, you should:</p>
-        <ul>
-          <li>
-            1. turn the infant on their back on the other forearm (head still
-            down),
-          </li>
-          <li>2. embrace the baby's occiput with your hand,</li>
-          <li>
-            3. indicate the place to compress the chest (lower half of the
-            sternum, 1 finger wide above the end),
-          </li>
-          <li>4. perform 5 chest compressions.</li>
-        </ul>
-        <p>
-          Then repeat both steps alternately (5 strokes and 5 compressions)
-          until the foreign body is removed.
-        </p>
-        <p>
-          If the infant loses consciousness, resuscitation is required! Start
-          with 5 breaths to push the foreign body deeper, even if it falls into
-          one of the lungs, it will allow the other lung to breathe.
-        </p>
-        <h4>How do you help a child over 1 year old?</h4>
-        <p>
-          In the event that the cough results, you should only encourage the
-          cough and watch the child. If the child is conscious but not coughing
-          or coughing is not effective, then:
-        </p>
-        <ul>
-          <li>1. stand behind the child,</li>
+            <li>
+              2. put your hand under the child's rib cage and bend them forward,
+            </li>
+            <li>
+              3. 5 back blows with the heel of your hand should be administered
+              into the inter-shoulder space,
+            </li>
+            <li>
+              4. after each back blow, check whether the foreign body has been
+              removed from the respiratory tract.
+            </li>
+          </ul>
+          <p>If the above actions fail, you should:</p>
+          <ul>
+            <li>
+              1. stand behind the child and put your arms around their upper
+              abdomen,
+            </li>
+            <li>2. lean the child forward</li>
+            <li>
+              3. put your fist, covered by the other hand, between the child’s
+              navel and breastbone,
+            </li>
+            <li>
+              4. press the epigastric region 5 times while moving inwards and
+              upwards, without applying pressure to the rib cage.
+            </li>
+          </ul>
+          <p>
+            Then repeat both steps alternately (5 strokes and 5 compressions)
+            until the foreign body is removed.
+          </p>
+          <h3>Nosebleed</h3>
+          <p>
+            Quite a common phenomenon, especially in young people. In this
+            situation
+          </p>
+          <ul>
+            <li>
+              1. convince the child to sit down with the head extended and
+              tilted forward,
+            </li>
+            <li>2. tighten both sides of the nose for 10 minutes,</li>
+            <li>
+              3. if blood runs down the throat, it should not be swallowed, it
+              must be spat out,
+            </li>
+            <li>
+              4. you can cool your child's neck and forehead with cold
+              compresses.
+            </li>
+          </ul>
+          <h3>Dealing with burns</h3>
+          <p>
+            In the event of a burn, the steps you should take are as follows:
+          </p>
+          <ul>
+            <li>1. remove clothing from the burned area,</li>
+            <li>
+              2. rinse the wound thoroughly with running water or, if this is
+              not possible, apply a hydrogel wrap,
+            </li>
+            <li>3. call for medical help or go to the hospital.</li>
+          </ul>
+          <h3>Preventing tick bites</h3>
+          <p>
+            Tick exposure can occur year-round, but ticks are most active during
+            warmer months (April-September).
+          </p>
 
-          <li>
-            2. put your hand under the child's rib cage and bend them forward,
-          </li>
-          <li>
-            3. 5 back blows with the heel of your hand should be administered
-            into the inter-shoulder space,
-          </li>
-          <li>
-            4. after each back blow, check whether the foreign body has been
-            removed from the respiratory tract.
-          </li>
-        </ul>
-        <p>If the above actions fail, you should:</p>
-        <ul>
-          <li>
-            1. stand behind the child and put your arms around their upper
-            abdomen,
-          </li>
-          <li>2. lean the child forward</li>
-          <li>
-            3. put your fist, covered by the other hand, between the child’s
-            navel and breastbone,
-          </li>
-          <li>
-            4. press the epigastric region 5 times while moving inwards and
-            upwards, without applying pressure to the rib cage.
-          </li>
-        </ul>
-        <p>
-          Then repeat both steps alternately (5 strokes and 5 compressions)
-          until the foreign body is removed.
-        </p>
-        <h3>Nosebleed</h3>
-        <p>
-          Quite a common phenomenon, especially in young people. In this
-          situation
-        </p>
-        <ul>
-          <li>
-            1. convince the child to sit down with the head extended and tilted
-            forward,
-          </li>
-          <li>2. tighten both sides of the nose for 10 minutes,</li>
-          <li>
-            3. if blood runs down the throat, it should not be swallowed, it
-            must be spat out,
-          </li>
-          <li>
-            4. you can cool your child's neck and forehead with cold compresses.
-          </li>
-        </ul>
-        <h3>Dealing with burns</h3>
-        <p>In the event of a burn, the steps you should take are as follows:</p>
-        <ul>
-          <li>1. remove clothing from the burned area,</li>
-          <li>
-            2. rinse the wound thoroughly with running water or, if this is not
-            possible, apply a hydrogel wrap,
-          </li>
-          <li>3. call for medical help or go to the hospital.</li>
-        </ul>
-        <h3>Preventing tick bites</h3>
-        <p>
-          Tick exposure can occur year-round, but ticks are most active during
-          warmer months (April-September).
-        </p>
+          <p>Before You Go Outdoors:</p>
+          <ul>
+            <li>
+              • Know where to expect ticks. Ticks live in grassy, brushy, or
+              wooded areas, or even on animals. Spending time outside walking
+              your dog, camping, gardening, or hunting could bring you in close
+              contact with ticks. Many people get ticks in their own yard or
+              neighborhood.
+            </li>
 
-        <p>Before You Go Outdoors:</p>
-        <ul>
-          <li>
-            • Know where to expect ticks. Ticks live in grassy, brushy, or
-            wooded areas, or even on animals. Spending time outside walking your
-            dog, camping, gardening, or hunting could bring you in close contact
-            with ticks. Many people get ticks in their own yard or neighborhood.
-          </li>
+            <li>
+              • Treat clothing and gear with products containing 0.5%
+              permethrin. Permethrin can be used to treat boots, clothing and
+              camping gear and remain protective through several washings.
+              Alternatively, you can buy permethrin-treated clothing and gear.
+            </li>
 
-          <li>
-            • Treat clothing and gear with products containing 0.5% permethrin.
-            Permethrin can be used to treat boots, clothing and camping gear and
-            remain protective through several washings. Alternatively, you can
-            buy permethrin-treated clothing and gear.
-          </li>
+            <li>• Use Oil of Lemon Eucalyptus (OLE)</li>
 
-          <li>• Use Oil of Lemon Eucalyptus (OLE)</li>
+            <li>
+              • Avoid Contact with Ticks - wooded and brushy areas with high
+              grass and leaf litter.
+            </li>
+          </ul>
+          <p>After You Come Indoors</p>
+          <ul>
+            <li>
+              Check your clothing for ticks. Ticks may be carried into the house
+              on clothing. Any ticks that are found should be removed. Tumble
+              dry clothes in a dryer on high heat for 10 minutes to kill ticks
+              on dry clothing after you come indoors. If the clothes are damp,
+              additional time may be needed. If the clothes require washing
+              first, hot water is recommended. Cold and medium temperature water
+              will not kill ticks.
+            </li>
+            <li>
+              Examine gear and pets. Ticks can ride into the home on clothing
+              and pets, then attach to a person later, so carefully examine
+              pets, coats, and daypacks.
+            </li>
+            <li>
+              Shower soon after being outdoors. Showering within two hours of
+              coming indoors has been shown to reduce your risk of getting Lyme
+              disease and may be effective in reducing the risk of other
+              tickborne diseases. Showering may help wash off unattached ticks
+              and it is a good opportunity to do a tick check.
+            </li>
+            <li>
+              Check your body for ticks after being outdoors. Conduct a full
+              body check upon return from potentially tick-infested areas,
+              including your own backyard. Use a hand-held or full-length mirror
+              to view all parts of your body. Check these parts of your body and
+              your child’s body for ticks:
+            </li>
+          </ul>
 
-          <li>
-            • Avoid Contact with Ticks - wooded and brushy areas with high grass
-            and leaf litter.
-          </li>
-        </ul>
-        <p>After You Come Indoors</p>
-        <ul>
-          <li>
-            Check your clothing for ticks. Ticks may be carried into the house
-            on clothing. Any ticks that are found should be removed. Tumble dry
-            clothes in a dryer on high heat for 10 minutes to kill ticks on dry
-            clothing after you come indoors. If the clothes are damp, additional
-            time may be needed. If the clothes require washing first, hot water
-            is recommended. Cold and medium temperature water will not kill
-            ticks.
-          </li>
-          <li>
-            Examine gear and pets. Ticks can ride into the home on clothing and
-            pets, then attach to a person later, so carefully examine pets,
-            coats, and daypacks.
-          </li>
-          <li>
-            Shower soon after being outdoors. Showering within two hours of
-            coming indoors has been shown to reduce your risk of getting Lyme
-            disease and may be effective in reducing the risk of other tickborne
-            diseases. Showering may help wash off unattached ticks and it is a
-            good opportunity to do a tick check.
-          </li>
-          <li>
-            Check your body for ticks after being outdoors. Conduct a full body
-            check upon return from potentially tick-infested areas, including
-            your own backyard. Use a hand-held or full-length mirror to view all
-            parts of your body. Check these parts of your body and your child’s
-            body for ticks:
-          </li>
-        </ul>
+          <p>
+            If you find a tick attached to your skin, there’s no need to
+            panic—the key is to remove the tick as soon as possible. There are
+            several tick removal devices on the market, but a plain set of
+            fine-tipped tweezers work very well.
+          </p>
 
-        <p>
-          If you find a tick attached to your skin, there’s no need to panic—the
-          key is to remove the tick as soon as possible. There are several tick
-          removal devices on the market, but a plain set of fine-tipped tweezers
-          work very well.
-        </p>
+          <h3>How to remove a tick</h3>
 
-        <h3>How to remove a tick</h3>
-
-        <ul>
-          <li>
-            Use fine-tipped tweezers to grasp the tick as close to the skin’s
-            surface as possible
-          </li>
-          <li>
-            Pull upward with steady, even pressure. Don’t twist or jerk the
-            tick; this can cause the mouth-parts to break off and remain in the
-            skin. If this happens, remove the mouth-parts with tweezers. If you
-            are unable to remove the mouth easily with clean tweezers, leave it
-            alone and let the skin heal.
-          </li>
-          <li>
-            After removing the tick, thoroughly clean the bite area and your
-            hands with rubbing alcohol or soap and water.
-          </li>
-          <li>
-            Never crush a tick with your fingers. Dispose of a live tick by
-            putting it in alcohol, placing it in a sealed bag/container,
-            wrapping it tightly in tape, or flushing it down the toilet.
-          </li>
-        </ul>
+          <ul>
+            <li>
+              Use fine-tipped tweezers to grasp the tick as close to the skin’s
+              surface as possible
+            </li>
+            <li>
+              Pull upward with steady, even pressure. Don’t twist or jerk the
+              tick; this can cause the mouth-parts to break off and remain in
+              the skin. If this happens, remove the mouth-parts with tweezers.
+              If you are unable to remove the mouth easily with clean tweezers,
+              leave it alone and let the skin heal.
+            </li>
+            <li>
+              After removing the tick, thoroughly clean the bite area and your
+              hands with rubbing alcohol or soap and water.
+            </li>
+            <li>
+              Never crush a tick with your fingers. Dispose of a live tick by
+              putting it in alcohol, placing it in a sealed bag/container,
+              wrapping it tightly in tape, or flushing it down the toilet.
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
