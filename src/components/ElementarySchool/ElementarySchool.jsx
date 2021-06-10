5e768923597206bdc079b66d08145cb89ee96473
Helena Ibarra <helena8ibarra@gmail.com>
2021-06-10 18:18:13 +0200

    elementary school dokončena

--- src/components/ElementarySchool/ElementarySchool.jsx
+++ src/components/ElementarySchool/ElementarySchool.jsx
@@ -4,82 +4,115 @@ import './style.css';
 const ElementarySchool = () => {
   return (
     <>
-      <div className="main__desc">
-        <div className="cover__photo--bike"></div>
-        <h2>Elementary school</h2>
-        <p>
-          School attendance in the Czech Republic is compulsory from the age of
-          6 to 15. Most children attend state schools, but there are also newly
-          established private and church schools. Education at state schools up
-          to 18 or 19 is free of charge but students at secondary schools have
-          to pay for their textbooks. Private and church schools charge school
-          fees. All schools are coeducational. Children in our school system do
-          not wear uniforms. Handicapped children are educated separately.
-        </p>
-        <p>
-          The school year starts on 1st September and ends on 30th June of the
-          following year. The school year is divided into two terms
-          (September-January, February-June). A school day is different at
-          different types of schools. The average number of lessons at a
-          secondary school is around thirty a week, primary schools have fewer
-          lessons, while specialised schools often have more. Classes begin
-          between 8 and 8.15 and there are from 4 to 6 lessons in a row,
-          followed by a lunch break, usually 45 minutes long, which is one
-          period, and then afternoon classes. Afternoon classes end between 4
-          and 5 at the latest. Breaks between the lessons last from 5 to 15
-          minutes.
-        </p>
-        <h3>How does it look in the typicall elementary public school?</h3>
-        <p>
-          School buildings in our country look very similar, only their size
-          differs depending on the number of pupils or students attending them.
-          They are usually large buildings with a few floors, often with a
-          sportsground attached. ln the basement or on the ground floor there
-          are cloakrooms with students' lockers, a boiler room, a workshop, a
-          school canteen, sometimes also a fitness centre, a snackbar, a
-          caretaker’s flat or a gymnasium. On the other floors there are usually
-          long hallways with many doors leading to classrooms, teachers'
-          offices, laboratories, a common room, the head's and deputy head's
-          offices, the administrative office, a school library, a computer room
-          and toilets. The hallways and staircases are decorated with pictures,
-          flowers and there are also notice-boards, some cabinets and
-          glasscases. All classrooms in the Czech Republic are almost the same.
-          There are large windows opposite the door, many of school desks and
-          chairs with two aisles in between, a blackboard and a shelf with
-          coloured and white chalk, a sponge and a cloth on the front wall, a
-          teacher’s desk, a bookcase, a notice board, a few pictures and a
-          portrait of the president, a wash-basin, a mirror, a thermometer and a
-          waste-paper basket. Subjects which need special equipment or aids are
-          taught in special classrooms such as a chemistry, biology or physics
-          laboratory, a music and an art room or a gymnasium. For teaching
-          foreign languages a language lab is specially equipped with various
-          audio-visual aids, such as maps, a tape or cassette recorder, a slide
-          or overhead projector, a screen and a video. Some schools also have an
-          assembly hall. What is a lesson in a Czech school like? After the
-          bell, when the teacher enters the room, the pupils or students stand
-          up to greet him. He makes an entry in the class register, marks absent
-          students and then he starts the lesson with revision of the previous
-          lesson. He examines the pupils individually by asking them to come to
-          the blackboard, they are asked to reckon, do an exercise, explain a
-          problem, respond to teacher’s questions or sometimes the whole class
-          takes a written test. The performance of the students who excel is
-          usually perfect, they are fluent and creative. Sometimes the
-          performance is rather disappointing both for the teacher and the
-          student alike. The reasons differ from not paying attention in class,
-          not doing homework regularly and copying it in the break before the
-          lesson, relying on one's pretty face, cutting classes and not working
-          consistently and systematically to spoiling one's performance due to
-          nervousness. The results range from excellent, very good, good, and
-          satisfactory to failure. After examination the teacher explains a new
-          subject matter and practises it with exercises. Before the end of the
-          lesson he sums up the topic and sets assignments for the next lesson.
-          The teachers are supposed to follow the curriculum but they are free
-          to choose textbooks for their students. Some students stay at school
-          after school hours and take part in after-school activities such as
-          singing in the choir, drama club, arts club, reciting club or games.)
-        </p>
-        <h3>How do I enroll my child to the public elementary school?</h3>
-        The tutorial, you can find <a href="#">here</a>
+      <section className="main__picture">
+        <img
+          className="elementary__pic"
+          src="/assets/img/educationImg/primaryschool.jpg"
+          alt="děti v základní škole"
+        />
+      </section>
+      <div className="elementarySchool">
+        <div className="main__desc">
+          <h2>Elementary school</h2>
+          <p>
+            School attendance in the Czech Republic is compulsory from the age
+            of 6 to 15. Most children attend state schools, but there are also
+            newly established private and church schools. Education at state
+            schools up to 18 or 19 is free of charge but students at secondary
+            schools have to pay for their textbooks.{' '}
+            <span>Private and church schools charge school fees.</span> All
+            schools are coeducational. Children in our school system do not wear
+            uniforms. Handicapped children are educated separately.
+          </p>
+          <p>
+            <span>
+              he school year starts on 1st September and ends on 30th June
+            </span>
+            T of the following year. The school year is divided into two terms
+            (September-January, February-June). A school day is different at
+            different types of schools. The average number of lessons at a
+            secondary school is around thirty a week, primary schools have fewer
+            lessons, while specialised schools often have more. Classes begin
+            between 8 and 8.15 and there are from 4 to 6 lessons in a row,
+            followed by a lunch break, usually 45 minutes long, which is one
+            period, and then afternoon classes. Afternoon classes end between 4
+            and 5 at the latest. Breaks between the lessons last from 5 to 15
+            minutes.
+          </p>
+          <h3>How does it look in the typicall elementary public school?</h3>
+          <p>
+            School buildings in our country look very similar, only their size
+            differs depending on the number of pupils or students attending
+            them. They are usually large buildings with a few floors, often with
+            a sportsground attached. ln the basement or on the ground floor
+            there are cloakrooms with students' lockers, a boiler room, a
+            workshop, a school canteen, sometimes also a fitness centre, a
+            snackbar, a caretaker’s flat or a gymnasium.
+          </p>
+          <p>
+            On the other floors there are usually long hallways with many doors
+            leading to classrooms, teachers' offices, laboratories, a common
+            room, the head's and deputy head's offices, the administrative
+            office, a school library, a computer room and toilets. The hallways
+            and staircases are decorated with pictures, flowers and there are
+            also notice-boards, some cabinets and glasscases.{' '}
+          </p>
+          <p>
+            All classrooms in the Czech Republic are almost the same. There are
+            large windows opposite the door, many of school desks and chairs
+            with two aisles in between, a blackboard and a shelf with coloured
+            and white chalk, a sponge and a cloth on the front wall, a teacher’s
+            desk, a bookcase, a notice board, a few pictures and a portrait of
+            the president, a wash-basin, a mirror, a thermometer and a
+            waste-paper basket.
+          </p>
+          <p>
+            Subjects which need special equipment or aids are taught in special
+            classrooms such as a chemistry, biology or physics laboratory, a
+            music and an art room or a gymnasium. For teaching foreign languages
+            a language lab is specially equipped with various audio-visual aids,
+            such as maps, a tape or cassette recorder, a slide or overhead
+            projector, a screen and a video. Some schools also have an assembly
+            hall.{' '}
+          </p>
+          <h3>What is a lesson in a Czech school like? </h3>
+          <p>
+            After the bell, when the teacher enters the room, the pupils or
+            students stand up to greet him. He makes an entry in the class
+            register, marks absent students and then he starts the lesson with
+            revision of the previous lesson. He examines the pupils individually
+            by asking them to come to the blackboard, they are asked to reckon,
+            do an exercise, explain a problem, respond to teacher’s questions or
+            sometimes the whole class takes a written test.
+          </p>
+          <p>
+            The performance of the students who excel is usually perfect, they
+            are fluent and creative. Sometimes the performance is rather
+            disappointing both for the teacher and the student alike. The
+            reasons differ from not paying attention in class, not doing
+            homework regularly and copying it in the break before the lesson,
+            relying on one's pretty face, cutting classes and not working
+            consistently and systematically to spoiling one's performance due to
+            nervousness.{' '}
+          </p>
+          <p>
+            The results range from excellent, very good, good, and satisfactory
+            to failure. After examination the teacher explains a new subject
+            matter and practises it with exercises. Before the end of the lesson
+            he sums up the topic and sets assignments for the next lesson.
+          </p>{' '}
+          <p>
+            The teachers are supposed to follow the curriculum but they are free
+            to choose textbooks for their students. Some students stay at school
+            after school hours and take part in after-school activities such as
+            singing in the choir, drama club, arts club, reciting club or
+            games.)
+          </p>
+          <h3>How do I enroll my child to the public elementary school?</h3>
+          <p>
+            The tutorial you can find <a href="#">here.</a>
+          </p>
+        </div>
       </div>
     </>
   );
