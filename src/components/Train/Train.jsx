fbbc867e02654bab0ad5acb7825e95b0ac26c064
Helena Ibarra <helena8ibarra@gmail.com>
2021-06-10 16:36:43 +0200

    travelling with a stroller by train upraveno

--- src/components/Train/Train.jsx
+++ src/components/Train/Train.jsx
@@ -4,70 +4,80 @@ import './style.css';
 const Train = () => {
   return (
     <>
-      <div className="main__desc">
-        <div className="cover__photo--bike"></div>
-        <h2>Travelling by train with children</h2>
-        <p>
-          No matter how old your children are, rail travel is a great way to get
-          out and about with them and explore. Whether they are toddlers needing
-          the space to move about on a long trip or teens after the free Wi-Fi
-          in first class, there is a good reason to travel by rail for every age
-          group. If you've never considered rail travel with your children
-          before, we hope this section will give you all the information and
-          advice you need.
-        </p>
-        <p>
-          Travelling with a baby can be a military operation– you'll want to
-          time the travel between feeds, ideally when baby has a nap and
-          hopefully between dirty nappies. You will also know that even with the
-          best will in the world, babies can be unpredictable. Planning is key
-          but so is being prepared for anything. If you're travelling by train
-          for the first time with your baby it's good to think ahead and be
-          equipped for the journey, especially if it's a long one!
-        </p>
-        <p>
-          There is something to be said about leaving the pram at home and
-          embracing the idea of a sling or baby carrier. It can be overwhelming
-          arriving at a train station and facing stairs, platforms and gaps; do
-          you keep the baby in, take the baby out, collapse the pram? Can you
-          ask for help down the stairs or will health and safety rules mean they
-          can't? A sling will eliminate all these worries and give you two free
-          hands to find your ticket, hold a drink or steady yourself on a moving
-          train. Travel light, try to deconstruct your usual changing bag and
-          take only what you need. Be prepared for nappy changes and illness,
-          have wipes and a change of clothes handy. Don't worry about upsetting
-          other passengers if your baby has an unscheduled cry, chances are
-          they've all been there or know someone who has.
-        </p>
-        <p>
-          If travelling with a buggy, it is advisable to collapse it and carry
-          it on separately but this is not always possible. Depending on what
-          train company you use, there is usually an area to park your buggy.
-        </p>
-        <p>
-          We would recommend getting to the station early so that when the train
-          arrives you can find the seats that face each other. This means your
-          children will be kicking you, as opposed to the customer in the seat
-          in front.
-        </p>
-        <p>
-          Travelling with a young child on a train requires pre-planning,
-          forward thinking and plenty of distractions. In most cases, the
-          initial excitement of travelling on a train is short lived, you will
-          be lucky if your little one is still enjoying the view five minutes
-          after departure, therefore it's important to have activities to fall
-          back on.
-        </p>
-        <p>
-          Don't rely on the snack cart to relieve the boredom and feed your
-          children; not all trains will have them so go prepared with food and
-          drink.
-        </p>
-        <p>
-          Children under six years can travel free with an adult, however be
-          aware there is a seat allocation so if a fare paying passenger needs
-          your child's seat, you must not give it to them.
-        </p>
+      <section className="main__picture">
+        <img
+          className="train__pic"
+          src="/assets/img/travellingImg/strollerTrain.png"
+          alt="cestování s kočárkem ve vlaku"
+        />
+      </section>
+      <div className="strollerTrain">
+        <div className="main__desc">
+          <h2>Travelling by train with children</h2>
+          <p>
+            No matter how old your children are, rail travel is a great way to
+            get out and about with them and explore. Whether they are toddlers
+            needing the space to move about on a long trip or teens after the
+            free Wi-Fi in first class, there is a good reason to travel by rail
+            for every age group. If you've never considered rail travel with
+            your children before, we hope this section will give you all the
+            information and advice you need.
+          </p>
+          <p>
+            Travelling with a baby can be a military operation– you'll want to
+            time the travel between feeds, ideally when baby has a nap and
+            hopefully between dirty nappies. You will also know that even with
+            the best will in the world, babies can be unpredictable. Planning is
+            key but so is being prepared for anything. If you're travelling by
+            train for the first time with your baby it's good to think ahead and
+            be equipped for the journey, especially if it's a long one!
+          </p>
+          <p>
+            There is something to be said about leaving the pram at home and
+            embracing the idea of a sling or baby carrier. It can be
+            overwhelming arriving at a train station and facing stairs,
+            platforms and gaps; do you keep the baby in, take the baby out,
+            collapse the pram? Can you ask for help down the stairs or will
+            health and safety rules mean they can't? A sling will eliminate all
+            these worries and give you two free hands to find your ticket, hold
+            a drink or steady yourself on a moving train. Travel light, try to
+            deconstruct your usual changing bag and take only what you need. Be
+            prepared for nappy changes and illness, have wipes and a change of
+            clothes handy. Don't worry about upsetting other passengers if your
+            baby has an unscheduled cry, chances are they've all been there or
+            know someone who has.
+          </p>
+          <p>
+            If travelling with a buggy, it is advisable to collapse it and carry
+            it on separately but this is not always possible. Depending on what
+            train company you use, there is usually an area to park your buggy.
+          </p>
+          <h3>Be prepared</h3>
+          <p>
+            We would recommend getting to the station early so that when the
+            train arrives you can find the seats that face each other. This
+            means your children will be kicking you, as opposed to the customer
+            in the seat in front.
+          </p>
+          <p>
+            Travelling with a young child on a train requires pre-planning,
+            forward thinking and plenty of distractions. In most cases, the
+            initial excitement of travelling on a train is short lived, you will
+            be lucky if your little one is still enjoying the view five minutes
+            after departure, therefore it's important to have activities to fall
+            back on.
+          </p>
+          <p>
+            Don't rely on the snack cart to relieve the boredom and feed your
+            children; not all trains will have them so go prepared with food and
+            drink.
+          </p>
+          <p>
+            <span>Children under six years can travel free</span> with an adult,
+            however be aware there is a seat allocation so if a fare paying
+            passenger needs your child's seat, you must not give it to them.
+          </p>
+        </div>
       </div>
     </>
   );
