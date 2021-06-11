104b8289297c96d2325b0c337c50c1cd7ae3118d
LucieKULHAVA <46672152+LucieKULHAVA@users.noreply.github.com>
2021-06-11 16:44:13 +0200

    add ujeyd

---
+++ src/components/Ujezd/Ujezd.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import './style.css';
+
+const Ujezd = () => (
+  <>
+   <div className="kurim__detail">
+  <div className="kurim__nazev">Újezd u Brna</div>
+                  <div className="kurim__podtitul">amazing educational forest path</div>
+                  <div className="kurim__popis">
+                   <p>
+                   Are you bored of playgrounds in Brno? Discover with us a new playground in Újezd ​​u Brna.
+This time we recommend a scooter with you.                   
+                   </p>
+                   <p>
+                   Distance: 3,1 km
+                   </p>
+                   <p>
+                   Duration of the walk with children: 5-6 hours /included playgrounds, farm, and pauses for refreshment/
+                   </p>
+                   <p>
+                    Stroller friendly: yes
+                   </p>
+                   <p>
+                    Map:
+                    <a href="Map: https://mapy.cz/s/jozetojuco">here</a>
+                   
+                   </p>
+                   <p>
+                    Playgrounds:
+                    The "Rozprýmova" playground
+                    The "Na zahrádkách" playground                   
+                   </p>
+                   <p>
+                    Open cafe: 
+                    Cukrárna at the main square (49.1046944N, 16.7571336E)                
+                   </p>
+                   <p>
+                    Getting to the start:
+                    By public transport: By train to the train stop "Újezd u Brna".
+                    By buses (no. 40, 151) to the bus stop "Újezd u Brna, Revoluční"                    
+                   </p>
+                   <p>
+                   More details and timetables:
+public transport (MHD) www.idos.cz
+                   </p>
+                   <p>
+                     Route description:
+                   </p>
+                   <p>
+                   After getting off the bus (stop "Újezd ​​u Brna, Revoluční") or train (stop Újezd ​​u Brna), turn right from the main road along Revoluční Street. Almost at the end of the street, you will come across the street "9. května". Turn onto this street. At the end of the road, you will see giant spruce. And with it, you will find the first playground. It is called the Bird Village. The climbing frames are in the shape of birdhouses, and the course resembles the Angry Birds series. In addition to the classic climbing frames, there is also a trampoline, rope obstacles or a sensory trail. You can spend the whole day here.
+If you want to get to one more playground, try it in Na Zahrádkách Street. This time it's about the sea, ships, sailors. It is smaller than a bird village and more suitable for more minor children. Even so, it's great.
+And if you need to walk even more around the village, don't forget to visit the farm. We posted the information about the "U Staré Hory" farm on our Facebook.
+You can use service no. 610 from the "Újezd u Brna,Rychmanov" bus stop (49.1035119N, 16.7652989E) for a way back to Brno.
+                    
+                   </p>
+
+                </div>
+              </div>
+
+              <div className="kurim__foto">
+            <div>
+              <img className="singlefoto" src="/assets/img/ujezd/1.jpg" alt="výlet újezd u brna"/>
+               
+            </div>
+            <div>
+            <img className="singlefoto" src="/assets/img/ujezd/2.jpg" alt="výlet újezd u brna"/>
+            </div>
+            <div>
+            <img className="singlefoto" src="/assets/img/ujezd/3.jpg" alt="výlet újezd u brna"/>
+            </div>
+           
+            </div>
+
+            <div className="kurim__foto">
+            <div>
+              <img className="singlefoto" src="/assets/img/ujezd/4.jpg" alt="výlet újezd u brna"/>
+               
+            </div>
+            <div>
+            <img className="singlefoto" src="/assets/img/ujezd/5.jpg" alt="výlet újezd u brna"/>
+            </div>
+            <div>
+            <img className="singlefoto" src="/assets/img/ujezd/6.jpg" alt="výlet újezd u brna"/>
+            </div>
+           
+            </div>
+              
+              
+         
+
+
+              </>
+  
+);
+
+export default Ujezd;
