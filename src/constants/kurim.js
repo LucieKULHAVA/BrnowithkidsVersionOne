8f02c5b5713fb687a3934b4dd1aaf6e21f47d83f
LucieKULHAVA <46672152+LucieKULHAVA@users.noreply.github.com>
2021-06-13 20:51:32 +0200

    uprava fotek

--- src/constants/kurim.js
+++ src/constants/kurim.js
@@ -1,26 +1,26 @@
 export const kurim = [
   {
     id: 1,
-    src: '/assets/img/kurim/kurim.jpg',
+    src: '/assets/img/kurim/1.jpg',
   },
   {
     id: 2,
-    src: '/assets/img/kurim/kurim.jpg',
+    src: '/assets/img/kurim/2.jpg',
   },
   {
     id: 3,
-    src: '/assets/img/kurim/kurim.jpg',
+    src: '/assets/img/kurim/3.jpg',
   },
   {
     id: 4,
-    src: '/assets/img/kurim/kurim.jpg',
+    src: '/assets/img/kurim/4.jpg',
   },
   {
     id: 5,
-    src: '/assets/img/kurim/kurim.jpg',
+    src: '/assets/img/kurim/5.jpg',
   },
   {
     id: 6,
-    src: '/assets/img/kurim/kurim.jpg',
+    src: '/assets/img/kurim/6.jpg',
   },
 ];
